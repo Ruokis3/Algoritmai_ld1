0fee594625c5f7dcdb353ae2994797843b97e3ab
Simonas <Ruokis3@users.noreply.github.com>
2020-04-22 13:50:15 +0300

    laptopui

--- myapp/Person.cs
+++ myapp/Person.cs
@@ -2,14 +2,14 @@ namespace myapp
 {
     class Person
     {
+        public int Heigth{get; set; }
+        public int Weight {get; set; }
+        public float BMI {get; set; }
         public Person(int heigth, int weight, float bmi)
         {
             int Heigth = heigth;
             int Weight = weight;
             float BMI = bmi;
-        }
-        public Person (){
-            
         }
     }
 }
