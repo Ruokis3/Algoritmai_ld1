f2bface27119c21f9bad553e9f1382f6dbeff838
Simonas <Ruokis3@users.noreply.github.com>
2020-04-22 11:29:21 +0300

    init

---
+++ myapp/Person.cs
@@ -0,0 +1,12 @@
+namespace myapp
+{
+    class Person
+    {
+        public Person(int heigth, int weight, float bmi)
+        {
+            int Heigth = heigth;
+            int Weight = weight;
+            float BMI = bmi;
+        }
+    }
+}
